cd34eb8e2498f59575fca440c4b6483bb32bcdbc
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-10-13 17:21:26 +0900

    feat: cors 설정
    모든 도메인에서 요청을 허용하도록 Cors 설정

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import redis
+
+app = FastAPI()
+
+# 모든 출처에서의 요청을 허용하기 위한 CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 모든 도메인에서 요청 허용
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용 (GET, POST, PUT, DELETE 등)
+    allow_headers=["*"],  # 모든 헤더 허용
+)
 
 # Redis 기본 엔드포인트 (샤드 0, 노드가 하나이므로 이 하나만 사용)
 REDIS_PRIMARY_ENDPOINT = "study-mentor-link-redis.g0cwuy.ng.0001.apn2.cache.amazonaws.com"
@@ -11,8 +23,6 @@ redis_client = redis.StrictRedis(
     port=REDIS_PORT,
     decode_responses=True  # 이 옵션은 바이트 데이터를 문자열로 자동 변환합니다.
 )
-
-app = FastAPI()
 
 @app.get("/")
 def read_root():
