fa0307c0bcb6cd4fa50952aeb1f390ad7fed7cab
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-10-13 17:35:02 +0900

    test: redis 읽기 쓰기 테스트
    redis 읽기 쓰기 테스트 코드 수정

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app.add_middleware(
 )
 
 # Redis 기본 엔드포인트 (샤드 0, 노드가 하나이므로 이 하나만 사용)
-REDIS_PRIMARY_ENDPOINT = "study-mentor-link-redis.g0cwuy.ng.0001.apn2.cache.amazonaws.com"
+#REDIS_PRIMARY_ENDPOINT = "study-mentor-link-redis.g0cwuy.ng.0001.apn2.cache.amazonaws.com"
+REDIS_PRIMARY_ENDPOINT = "study-mentor-link-redis-001.g0cwuy.0001.apn2.cache.amazonaws.com"
 REDIS_PORT = 6379
 
 # Redis 클라이언트 초기화
@@ -24,12 +25,12 @@ redis_client = redis.StrictRedis(
     decode_responses=True  # 이 옵션은 바이트 데이터를 문자열로 자동 변환합니다.
 )
 
-@app.get("/")
-def read_root():
-    # Redis에 데이터 쓰기
-    redis_client.set("message", "Hello from Redis!")
-    
-    # Redis에서 데이터 읽기
-    message = redis_client.get("message")
-    
+@app.get("/write")
+def write_to_redis():
+    redis_client.set("write_message", "This is written to the primary")
+    return {"status": "written"}
+
+@app.get("/read")
+def read_from_redis():
+    message = redis_client.get("write_message")
     return {"message": message}
