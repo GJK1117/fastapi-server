27c63099968d96260b555f89833ed92b9cd49148
GJK1117 <118426175+GJK1117@users.noreply.github.com>
2024-10-15 06:54:34 +0900

    fix: APIRouter 구조 변경
    - 라우터 하나마다 파일 하나로 관리하여 더 세분화하게 관리
    - 라우터 파일이름은 라우터 주소 마지막에 붙는 키워드

--- app/api/auth/__init__.py
+++ app/api/auth/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
-from app.api.auth.routers import router
+from app.api.auth.email import router as email_router
+from app.api.auth.num import router as num_router
 
 api_router=APIRouter()
-api_router.include_router(router, prefix="/auth", tags=["auth"])
+api_router.include_router(email_router, prefix="/email", tags=["auth", "email"])
+api_router.include_router(num_router, prefix="/num", tags=["auth", "num"])
